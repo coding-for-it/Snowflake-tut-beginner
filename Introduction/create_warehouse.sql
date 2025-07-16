bf62f808990e2ddf3fa7e007e32ea384d303c5a5
Nimisha <codeforit.rn.17@gmail.com>
2025-07-16 17:12:50 +0530

    initial commit

--- Introduction/create_warehouse.sql
+++ Introduction/create_warehouse.sql
@@ -7,5 +7,16 @@ AUTO_RESUME = TRUE;
 --activate warehouse
 USE WAREHOUSE beginner_wh;
 
+--to pause warehouse(to save money)
+ALTER WAREHOUSE beginner_wh SUSPEND;
+
+--to resume
+ALTER WAREHOUSE beginner_wh RESUME;
+
+SHOW WAREHOUSES;
+
 --show warehouses
 SHOW WAREHOUSES;
+
+--drop warehouse
+DROP WAREHOUSE IF EXISTS beginner_wh;
